2100803f0b6333d208ae7be44e9aeb675be2524f
Matthew Ma <mxp.show@gmail.com>
2019-08-05 22:10:22 -0700

    Initial Commit

---
+++ ShortCuts/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  ShortCuts
+//
+//  Created by Xiupeng Ma on 8/5/19.
+//  Copyright © 2019 Xiupeng Ma. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ ShortCuts/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  ShortCuts
+//
+//  Created by Xiupeng Ma on 8/5/19.
+//  Copyright © 2019 Xiupeng Ma. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+

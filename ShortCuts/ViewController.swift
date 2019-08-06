52f5074ba18afb9d191b74208f648eeb1f24e43d
Matthew Ma <mxp.show@gmail.com>
2019-08-06 00:54:03 -0700

    init. Build a prototype

--- ShortCuts/ViewController.swift
+++ ShortCuts/ViewController.swift
@@ -12,7 +12,32 @@ class ViewController: NSViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        if type(of: self).hasA11yPrivileges() {
+            print("Has a11y privileges");
+        } else {
+            print("Has No privilesge");
+            if !type(of: self).hasA11yPrivileges() {
+                return
+            }
+        }
+        
+        NSEvent.addGlobalMonitorForEvents(matching: .keyDown) { (event) in
+//            print(event)
+            if event.modifierFlags.contains(.command) &&
+                event.modifierFlags.contains(.control)
+                {
+                    if event.keyCode == 38 {
+                        self.getRunningApplications("chrome")
+                    } else if event.keyCode == 40 {
+                        self.getRunningApplications("youdao")
+                    } else if event.keyCode == 37 {
+                        self.getRunningApplications("mpv")
+                    }
+//                    print(event.keyCode)
+                    
+            }
+        }
         // Do any additional setup after loading the view.
     }
 
@@ -21,7 +46,35 @@ class ViewController: NSViewController {
         // Update the view, if already loaded.
         }
     }
-
-
+    
+    static func hasA11yPrivileges() -> Bool{
+        let trusted = kAXTrustedCheckOptionPrompt.takeUnretainedValue()
+        let privOptions = [trusted: true] as CFDictionary
+        let accessEnabled = AXIsProcessTrustedWithOptions(privOptions)
+        return accessEnabled
+    }
+    
+    func getRunningApplications(_ key: String) {
+        let runningApps = NSWorkspace.shared.runningApplications
+        var chrome: NSRunningApplication?
+        for app in runningApps {
+            if app.localizedName?.lowercased().contains(key) ?? false {
+                chrome = app
+                break
+            }
+        }
+//        if let c = chrome {
+//            NSWorkspace.shared.activate
+//            NSWorkspace.shared.frontmostApplication(chrome)
+//        }
+        if let c = chrome {
+//            print(c)
+//            print(c.activate(options: .activateIgnoringOtherApps))
+            c.activate(options: .activateIgnoringOtherApps)
+            if c.isHidden {
+                c.unhide()
+            }
+        }
+    }
 }
 
